1e064b0067f76e54af415ea64c450b36c7d77f67
Bang(Max) Nguyen <38081550+BangNguyenSJSU@users.noreply.github.com>
2022-02-14 18:04:51 -0800

    Finished print out format ( Ecat + Meter) Section
    ---> need to work on the deviation Ecat section
    ---> Need Meter Deviation need to add 1%
    ---> Start to outline the functions that perform error counting ( Ecat + Meter )

--- ReprotAnalysis/ReprotAnalysis/Program.cs
+++ ReprotAnalysis/ReprotAnalysis/Program.cs
@@ -17,12 +17,19 @@ namespace ReportAnalysis
         {
             Console.WriteLine("Bicep Report Analysis Test ");
 
-            string pathToSerialNumberFolder = @"C:\Bicep_Project\ReprotApplication\Bicep_Data\SN_22040007_FAIL2 _STATION1\Main_FULL.csv";
+            string pathToSerialNumberFolder = @"C:\Users\bang.nguyen\OneDrive - XP Power\Desktop\Bicept project\ReprotApplication\Bicep_Data\SN_22040007_FAIL2 _STATION1\Main_FULL.csv";
             string setpoint8kv = "";
             string setpoint3kv = "";
 
 
-            string table_boarder= "|///////////////////////////////////////////////////////////////////////////////|\n";
+            EcatReading objEcat = new EcatReading(pathToSerialNumberFolder, "sampple");
+
+            objEcat.GetEcatBoundary();
+            objEcat.ConvertEcatReadingInto_Double();
+            objEcat.GetEcatResult();
+
+
+            string table_boarder= "|///////////////////////////////////////////////////////////////////////////////////////////|\n";
            
 
             MeterReading objMeter = new MeterReading(pathToSerialNumberFolder, "sampple");
@@ -35,43 +42,53 @@ namespace ReportAnalysis
                
                 int errorCounter = 0;
 
-                
-                Double POS_Dev =0.000;
-                Double POS_Dev_Percent = 0.000;
+                // --------- Meter Section ---------
+                Double Meter_POS_Dev =0.000;
+                Double Meter_POS_Dev_Percent = 0.000;
 
-                Double NEG_Dev =0.000;
-                Double NEG_Dev_Percent =0.000;
+                Double Meter_NEG_Dev = 0.000;
+                Double Meter_NEG_Dev_Percent = 0.000;
 
-                Double BIAS_Dev = 0.000;
-                Double BIAS_Dev_Percent = 0.000;
+                Double Meter_BIAS_Dev = 0.000;
+                Double Meter_BIAS_Dev_Percent = 0.000;
+
+                // --------- Ecat Section ---------
+                Double Ecat_POS_Dev = 0.000;
+                Double Ecat_POS_Dev_Percent = 0.000;
+
+                Double Ecat_NEG_Dev = 0.000;
+                Double Ecat_NEG_Dev_Percent = 0.000;
+
+                Double Ecat_BIAS_Dev = 0.000;
+                Double Ecat_BIAS_Dev_Percent = 0.000;
 
 
                 // ------------------ V Postive 
                 if (objMeter.MeterResultVoutPOS[i].Contains("LOW"))
                 {
-                    POS_Dev = Math.Abs(objMeter.MeterVoutPOS_L[i]) - Math.Abs(objMeter.MeterVoutPOS[i]);
-                    POS_Dev_Percent = Math.Round((POS_Dev*100)/ Math.Abs(objMeter.MeterVoutPOS_L[i]), 3);
+                    Meter_POS_Dev = Math.Abs(objMeter.MeterVoutPOS_L[i]) - Math.Abs(objMeter.MeterVoutPOS[i]);
+                    Meter_POS_Dev_Percent = Math.Round((Meter_POS_Dev*100)/ Math.Abs(objMeter.MeterVoutPOS_L[i]), 3);
 
                     errorCounter++;
                 }
                 else if (objMeter.MeterResultVoutPOS[i].Contains("HIGH"))
                 {
-                    POS_Dev = Math.Abs(objMeter.MeterVoutPOS[i]) - Math.Abs(objMeter.MeterVoutPOS_H[i]);
-                    POS_Dev_Percent = Math.Round((POS_Dev * 100) / Math.Abs(objMeter.MeterVoutPOS_H[i]), 3);
+                    Meter_POS_Dev = Math.Abs(objMeter.MeterVoutPOS[i]) - Math.Abs(objMeter.MeterVoutPOS_H[i]);
+                    Meter_POS_Dev_Percent = Math.Round((Meter_POS_Dev * 100) / Math.Abs(objMeter.MeterVoutPOS_H[i]), 3);
                     errorCounter++;
                 }
 
                 // ------------------- V Negative
                 if (objMeter.MeterResultVoutNEG[i].Contains("LOW"))
                 {
-                    NEG_Dev = Math.Abs(objMeter.MeterVoutNEG_L[i]) - Math.Abs(objMeter.MeterVoutNEG[i]);
-                    NEG_Dev_Percent = Math.Round((NEG_Dev * 100) / Math.Abs(objMeter.MeterVoutNEG_L[i]), 3);
+                    Meter_NEG_Dev = Math.Abs(objMeter.MeterVoutNEG_L[i]) - Math.Abs(objMeter.MeterVoutNEG[i]);
+                    Meter_NEG_Dev_Percent = Math.Round((Meter_NEG_Dev * 100) / Math.Abs(objMeter.MeterVoutNEG_L[i]), 3);
                     errorCounter++;
                 }
                 else if (objMeter.MeterResultVoutNEG[i].Contains("HIGH"))
                 {
-                    NEG_Dev = Math.Abs(objMeter.MeterVoutNEG[i]) - Math.Abs(objMeter.MeterVoutNEG_H[i]);
-                    NEG_Dev_Percent = Math.Round((NEG_Dev * 100) / Math.Abs(objMeter.MeterVoutNEG_H[i]), 3);
+                    Meter_NEG_Dev = Math.Abs(objMeter.MeterVoutNEG[i]) - Math.Abs(objMeter.MeterVoutNEG_H[i]);
+                    Meter_NEG_Dev_Percent = Math.Round((Meter_NEG_Dev * 100) / Math.Abs(objMeter.MeterVoutNEG_H[i]), 3);
                     errorCounter++;
                 }
 
@@ -80,88 +97,152 @@ namespace ReportAnalysis
                 {
                     if (objMeter.MeterResultVbias[i].Contains("LOW"))
                     {
-                        BIAS_Dev = (objMeter.BiasSetPoint[i] * 0.99) - objMeter.BiasSetPoint[i];
+                        Meter_BIAS_Dev = (objMeter.BiasSetPoint[i] * 0.99) - objMeter.BiasSetPoint[i];
                         errorCounter++;
 
                     }
                     else if (objMeter.MeterResultVbias[i].Contains("HIGH"))
                     {
-                        BIAS_Dev = (objMeter.BiasSetPoint[i] * 1.01) - objMeter.BiasSetPoint[i];
+                        Meter_BIAS_Dev = (objMeter.BiasSetPoint[i] * 1.01) - objMeter.BiasSetPoint[i];
                         errorCounter++;
                     }
                     
                 }
 
-               
 
+                //==================================================================
+                //                Meter Convert to string section
+                //==================================================================
                 // Setpoint  [Clamp , Bias]
                 string Clamp = Convert.ToString(objMeter.ClampSetPoint[i]).PadRight(5);
                 string Bias = Convert.ToString(objMeter.BiasSetPoint[i]).PadRight(4);
 
-          
+
                 // POSITIVE Convert to String with alignment 
-                string vPOS_L = Convert.ToString(objMeter.MeterVoutPOS_L[i]).PadRight(7);
-                string vPOS = Convert.ToString(objMeter.MeterVoutPOS[i]).PadRight(6);
-                string vPOS_H = Convert.ToString(objMeter.MeterVoutPOS_H[i]).PadRight(7);
-                string vPOS_R = objMeter.MeterResultVoutPOS[i].PadRight(10);
-                string POS_D = Convert.ToString(POS_Dev).PadRight(3);
-                string POS_D_P = Convert.ToString(POS_Dev_Percent).PadRight(5);
+                string Meter_vPOS_L = Convert.ToString(objMeter.MeterVoutPOS_L[i]).PadRight(9);
+                string Meter_vPOS = Convert.ToString(objMeter.MeterVoutPOS[i]).PadRight(8);
+                string Meter_vPOS_H = Convert.ToString(objMeter.MeterVoutPOS_H[i]).PadRight(9);
+                string Meter_vPOS_R = objMeter.MeterResultVoutPOS[i].PadRight(10);
+                string Meter_POS_D = Convert.ToString(Meter_POS_Dev).PadRight(3);
+                string Meter_POS_D_P = Convert.ToString(Meter_POS_Dev_Percent).PadRight(5);
 
                 // NEGATIVE Convert to String with alignment 
-                string vNEG_L = Convert.ToString(objMeter.MeterVoutNEG_L[i]).PadRight(7);
-                string vNEG = Convert.ToString(objMeter.MeterVoutNEG[i]).PadRight(6);
-                string vNEG_H = Convert.ToString(objMeter.MeterVoutNEG_H[i]).PadRight(7);
-                string vNEG_R = objMeter.MeterResultVoutNEG[i].PadRight(10);
-                string NEG_D = Convert.ToString(NEG_Dev).PadRight(3);
-                string NEG_D_P = Convert.ToString(NEG_Dev_Percent).PadRight(5);
+                string Meter_vNEG_L = Convert.ToString(objMeter.MeterVoutNEG_L[i]).PadRight(9);
+                string Meter_vNEG = Convert.ToString(objMeter.MeterVoutNEG[i]).PadRight(8);
+                string Meter_vNEG_H = Convert.ToString(objMeter.MeterVoutNEG_H[i]).PadRight(9);
+                string Meter_vNEG_R = objMeter.MeterResultVoutNEG[i].PadRight(10);
+                string Meter_NEG_D = Convert.ToString(Meter_NEG_Dev).PadRight(3);
+                string Meter_NEG_D_P = Convert.ToString(Meter_NEG_Dev_Percent).PadRight(5);
+
+                // VBIAS Convert to String with alignment
+                string Meter_vBias_L = Convert.ToString(objMeter.BiasSetPoint[i] * 0.99).PadRight(9);
+                string Meter_vBias = Convert.ToString(objMeter.MeterVbias[i]).PadRight(8);
+                string Meter_vBias_H = Convert.ToString(objMeter.BiasSetPoint[i] * 1.01).PadRight(9);
+                string Meter_vBias_R = Convert.ToString(objMeter.MeterResultVbias[i]).PadRight(10);
+                string Meter_Bias_D = Convert.ToString(Meter_BIAS_Dev).PadRight(3);
+                string Meter_Bias_D_P = Convert.ToString(Meter_BIAS_Dev_Percent).PadRight(5);
+
+                // VDIFF Convert to String with alignment 
+                string Meter_vDIF_N = Convert.ToString(objMeter.MeterVoutNEG[i]).PadRight(9);
+                string Meter_vDIF = Convert.ToString(objMeter.MeterVdiff[i]).PadRight(8);
+                string Meter_vDIF_P = Convert.ToString(objMeter.MeterVoutPOS[i]).PadRight(9);
+
+                //==================================================================
+                //                Ecat Convert to string section
+                //==================================================================
+
+
+                // POSITIVE Convert to String with alignment 
+                string Ecat_vPOS_L = Convert.ToString(objEcat.EcatVoutPOS_L[i]).PadRight(9);
+                string Ecat_vPOS = Convert.ToString(objEcat.EcatVoutPOS[i]).PadRight(8);
+                string Ecat_vPOS_H = Convert.ToString(objEcat.EcatVoutPOS_H[i]).PadRight(9);
+                string Ecat_vPOS_R = objEcat.EcatResultVoutPOS[i].PadRight(10);
+                string Ecat_POS_D = Convert.ToString(Ecat_POS_Dev).PadRight(3);
+                string Ecat_POS_D_P = Convert.ToString(Ecat_POS_Dev_Percent).PadRight(5);
+
+                // NEGATIVE Convert to String with alignment 
+                string Ecat_vNEG_L = Convert.ToString(objEcat.EcatVoutNEG_L[i]).PadRight(9);
+                string Ecat_vNEG = Convert.ToString(objEcat.EcatVoutNEG[i]).PadRight(8);
+                string Ecat_vNEG_H = Convert.ToString(objEcat.EcatVoutNEG_H[i]).PadRight(9);
+                string Ecat_vNEG_R = objEcat.EcatResultVoutNEG[i].PadRight(10);
+                string Ecat_NEG_D = Convert.ToString(Ecat_NEG_Dev).PadRight(3);
+                string Ecat_NEG_D_P = Convert.ToString(Ecat_NEG_Dev_Percent).PadRight(5);
+
+                // VBIAS Convert to String with alignment
+                string Ecat_vBias_L = Convert.ToString(objEcat.BiasSetPoint[i] * 0.99).PadRight(9);
+                string Ecat_vBias = Convert.ToString(objEcat.EcatCTout[i]).PadRight(8);
+                string Ecat_vBias_H = Convert.ToString(objEcat.BiasSetPoint[i] * 1.01).PadRight(9);
+                string Ecat_vBias_R = Convert.ToString(objEcat.EcatResultCTout[i]).PadRight(10);
+                string Ecat_Bias_D = Convert.ToString(Ecat_BIAS_Dev).PadRight(3);
+                string Ecat_Bias_D_P = Convert.ToString(Ecat_BIAS_Dev_Percent).PadRight(5);
+
+                // VDIFF Convert to String with alignment ( Vdiff LOW and Vdiff HIGH)
+                string Ecat_vDIF_L = Convert.ToString(objEcat.EcatVdiff_L[i]).PadRight(9);
+                string Ecat_vDIF = Convert.ToString(objEcat.EcatVdiff[i]).PadRight(8);
+                string Ecat_vDIF_H = Convert.ToString(objEcat.EcatVdiff_H[i]).PadRight(9);
+                string Ecat_vDIF_R = objEcat.EcatResultVdiff[i].PadRight(10);
+
 
 
                 if (errorCounter != 0)
                 {
-                    // VBIAS Convert to String with alignment
-                    string vBias_L = Convert.ToString(objMeter.BiasSetPoint[i] * 0.99).PadRight(7);
-                    string vBias = Convert.ToString(objMeter.MeterVbias[i]).PadRight(6);
-                    string vBias_H = Convert.ToString(objMeter.BiasSetPoint[i] * 1.01).PadRight(7);
-                    string vBias_R = Convert.ToString(objMeter.MeterResultVbias[i]).PadRight(10);
-                    string Bias_D = Convert.ToString(BIAS_Dev).PadRight(3);
-                    string Bias_D_P = Convert.ToString(BIAS_Dev_Percent).PadRight(5);
-
-                    // VDIFF Convert to String with alignment 
-                    string vDIF_N = Convert.ToString(objMeter.MeterVoutNEG[i]).PadRight(7);
-                    string vDIF = Convert.ToString(objMeter.MeterVdiff[i]).PadRight(6);
-                    string vDIF_P = Convert.ToString(objMeter.MeterVoutPOS[i]).PadRight(7);
 
 
+                    //==================================================================
+                    //                Meter Convert to string section
+                    //==================================================================
 
 
                     // Setpoint
-                    Console.WriteLine($"|--------------------[{Clamp},{Bias}]---------------------------------|-[Deviation]-|");
+                    Console.WriteLine($"|--------------------[{Clamp},{Bias}]---------------------------|-[Result]--|----[Deviation]----|");
 
                     // Positive
-                    Console.WriteLine($"| V(+)_LOW --> {vPOS_L}  {vPOS}  {vPOS_H} <-- V(+)_HIGH | {vPOS_R}| {POS_D} ({POS_D_P}%)|");
+                    Console.WriteLine($"| V(+)_LOW --> {Meter_vPOS_L}  {Meter_vPOS}  {Meter_vPOS_H} <-- V(+)_HIGH | {Meter_vPOS_R}| Volt: {Meter_POS_D} ({Meter_POS_D_P}%)|");
 
                     // Negative 
-                    Console.WriteLine($"| V(-)_LOW --> {vNEG_L}  {vNEG}  {vNEG_H} <-- V(-)_HIGH | {vNEG_R}| {NEG_D} ({NEG_D_P}%)|");
+                    Console.WriteLine($"| V(-)_LOW --> {Meter_vNEG_L}  {Meter_vNEG}  {Meter_vNEG_H} <-- V(-)_HIGH | {Meter_vNEG_R}| Volt: {Meter_NEG_D} ({Meter_NEG_D_P}%)|");
 
                     // Vbias 
-                    Console.WriteLine($"| Vbias_LOW -> {vBias_L}  {vBias}  {vBias_H} <- Vbias_HIGH | {vBias_R}| {Bias_D} ({Bias_D_P}%)|");
+                    Console.WriteLine($"| Vbias_LOW -> {Meter_vBias_L}  {Meter_vBias}  {Meter_vBias_H} <- Vbias_HIGH | {Meter_vBias_R}| Volt: {Meter_Bias_D} ({Meter_Bias_D_P}%)|");
 
 
-                    Console.WriteLine("|-----------------Vdiff Computation---------------------------------------------|");
+                    Console.WriteLine("|----------------------vDiff METER----------------------------------------------------------|");
                     //Vdiff
-                    Console.WriteLine($"| V(-)      -> {vDIF_N}  {vDIF}  {vDIF_P} <-       V(+) |-------------------------|  ");
+                    Console.WriteLine($"| V(-)      -> {Meter_vDIF_N}  {Meter_vDIF}  {Meter_vDIF_P} <-       V(+) |-------------------------------|  ");
+
+
+                    Console.WriteLine(("-------------------------ECAT SECTION-------------------------------------------------------|"));
+
+                    // Positive
+                    Console.WriteLine($"| V(+)_LOW --> {Ecat_vPOS_L}  {Ecat_vPOS}  {Ecat_vPOS_H} <-- V(+)_HIGH | {Ecat_vPOS_R}| Volt: {Ecat_POS_D} ({Ecat_POS_D_P}%)|");
+
+                    // Negative 
+                    Console.WriteLine($"| V(-)_LOW --> {Ecat_vNEG_L}  {Ecat_vNEG}  {Ecat_vNEG_H} <-- V(-)_HIGH | {Ecat_vNEG_R}| Volt: {Ecat_NEG_D} ({Ecat_NEG_D_P}%)|");
+
+                    // Vbias 
+                    Console.WriteLine($"| CTout_LOW -> {Ecat_vBias_L}  {Ecat_vBias}  {Ecat_vBias_H} <- CTout_HIGH | {Ecat_vBias_R}| Volt: {Ecat_Bias_D} ({Ecat_Bias_D_P}%)|");
+
+                   
+
+                    //Console.WriteLine("|-----------------vDiff ECAT----------------------------------------------------------------|");
+                    //Vdiff
+                    Console.WriteLine($"| vDiff_L   -> {Ecat_vDIF_L}  {Ecat_vDIF}  {Ecat_vDIF_H} <-    vDiff_H | {Ecat_vDIF_R}|-------------------|  ");
 
                     Console.WriteLine(table_boarder);
+                    Console.WriteLine((" "));
+
+
+                    //==================================================================
+                    //                Ecat Convert to string section
+                    //==================================================================
+
+
+
                 }
 
 
             }
 
-            EcatReading objEcat = new EcatReading(pathToSerialNumberFolder, "sampple");
-
-            objEcat.GetEcatBoundary();
-            objEcat.ConvertEcatReadingInto_Double();
-            objEcat.GetEcatResult();
 
             for (int i = 0; i < objEcat.EcatVoutNEG.Length; i++)
             {
